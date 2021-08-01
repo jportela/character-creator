2acee036fb4fe91219999730dc66cef36f8ba8c1
Joao Portela <joao.portela@gmail.com>
2021-08-01 16:43:48 +0100

    handling 1-4 exercises

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import SecondBackground from './components/SecondBackground';
 
 function App() {
   return (
     <>
+      <SecondBackground />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/SecondBackground/SecondBackground.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './SecondBackground.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+};
+
+export default Footer;
